47f18fe3717305f4471f32e16393ee4fe1431e85
KiwisDev <gaelvolatier@gmail.com>
2024-03-13 15:35:17 +0100

    Delete TextureSheet2D

--- sprite_renderer.cpp
+++ sprite_renderer.cpp
@@ -68,30 +68,3 @@ void SpriteRenderer::drawSpite(Texture2D* texture, glm::vec2 position, glm::vec2
     glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
     glBindVertexArray(0);
 }
-
-void SpriteRenderer::drawSpite(TextureSheet2D* texture, int id, glm::vec2 position, glm::vec2 size, float rotate, glm::vec3 color) {
-    this->shader->use();
-
-    glm::mat4 model = glm::mat4(1.0f);
-
-    model = glm::translate(model, glm::vec3(position, 0.0));
-    model = glm::translate(model, glm::vec3(0.5f * size.x, 0.5f * size.y, 0.0f));
-    model = glm::rotate(model, glm::radians(rotate), glm::vec3(0.0f, 0.0f, 1.0f));
-    model = glm::translate(model, glm::vec3(-0.5f * size.x, -0.5f * size.y, 0.0f));
-    model = glm::scale(model, glm::vec3(size, 1.0f));
-
-    this->shader->setMat4("model", model);
-    this->shader->setVec3("spriteColor", color);
-
-    glActiveTexture(GL_TEXTURE0);
-    texture->use();
-    texture->updateVertices(vertices, id);
-
-    glBindBuffer(GL_ARRAY_BUFFER, this->quadVBO);
-    glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-    glBindVertexArray(this->quadVAO);
-    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
-    glBindVertexArray(0);
-}
--- sprite_renderer.h
+++ sprite_renderer.h
@@ -6,7 +6,6 @@
 
 #include "shader.h"
 #include "texture2D.h"
-#include "texture_sheet2D.h"
 
 class SpriteRenderer {
 private:
@@ -17,7 +16,6 @@ public:
 	SpriteRenderer(Shader* shader);
 
 	void drawSpite(Texture2D* texture, glm::vec2 position, glm::vec2 size = glm::vec2(10.0f, 10.0f), float rotate = 0.0f, glm::vec3 color = glm::vec3(1.0f));
-	void drawSpite(TextureSheet2D* texture, int id, glm::vec2 position, glm::vec2 size = glm::vec2(10.0f, 10.0f), float rotate = 0.0f, glm::vec3 color = glm::vec3(1.0f));
 };
 
 #endif
